6e0af346d22ad7d68e3f6e010f1ad721055c5213
Richard Dancsi <wimagguc@gmail.com>
2012-10-05 16:12:36 +0200

    Added: receive elevation value

--- js/jquery-gmaps-latlon-picker.js
+++ js/jquery-gmaps-latlon-picker.js
@@ -24,6 +24,7 @@ var GMapsLatLonPicker = (function() {
 		defLng : 0,
 		defZoom : 1,
 		queryLocationNameWhenLatLngChanges: true,
+		queryElevationWhenLatLngChanges: true,
 		mapOptions : {
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
 			mapTypeControl: false,
@@ -51,8 +52,8 @@ var GMapsLatLonPicker = (function() {
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	// PRIVATE FUNCTIONS FOR MANIPULATING DATA ////////////////////////////////////////////////////
-    var setPosition = function(position) {
-    	_self.vars.marker.setPosition(position);
+	var setPosition = function(position) {
+		_self.vars.marker.setPosition(position);
 		_self.vars.map.panTo(position);
 
 		$(_self.vars.cssID + ".gllpZoom").val( _self.vars.map.getZoom() );
@@ -64,22 +65,48 @@ var GMapsLatLonPicker = (function() {
 		if (_self.params.queryLocationNameWhenLatLngChanges) {
 			getLocationName(position);
 		}
-    };
-    
-    // for reverse geocoding
-    var getLocationName = function(position) {
+		if (_self.params.queryElevationWhenLatLngChanges) {
+			getElevation(position);
+		}
+	};
+	
+	// for reverse geocoding
+	var getLocationName = function(position) {
 		var latlng = new google.maps.LatLng(position.lat(), position.lng());
-    	_self.vars.geocoder.geocode({'latLng': latlng}, function(results, status) {
-    		if (status == google.maps.GeocoderStatus.OK && results[1]) {
-          		$(_self.vars.cssID + ".gllpLocationName").val(results[1].formatted_address);
-          	} else {
-          		$(_self.vars.cssID + ".gllpLocationName").val("");
-          	}
+		_self.vars.geocoder.geocode({'latLng': latlng}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK && results[1]) {
+				$(_self.vars.cssID + ".gllpLocationName").val(results[1].formatted_address);
+			} else {
+				$(_self.vars.cssID + ".gllpLocationName").val("");
+			}
+			$(_self.vars.cssID).trigger("location_name_changed", $(_self.vars.cssID));
 		});
-    };
+	};
 
+	// for getting the elevation value for a position
+	var getElevation = function(position) {
+		var latlng = new google.maps.LatLng(position.lat(), position.lng());
+
+		var locations = [latlng];
+
+		var positionalRequest = { 'locations': locations };
+
+		_self.vars.elevator.getElevationForLocations(positionalRequest, function(results, status) {
+			if (status == google.maps.ElevationStatus.OK) {
+				if (results[0]) {
+					$(_self.vars.cssID + ".gllpElevation").val( results[0].elevation.toFixed(3));
+				} else {
+					$(_self.vars.cssID + ".gllpElevation").val("");
+				}
+			} else {
+				$(_self.vars.cssID + ".gllpElevation").val("");
+			}
+			$(_self.vars.cssID).trigger("elevation_changed", $(_self.vars.cssID));
+		});
+	};
+	
 	// search function
-    var performSearch = function(string, silent) {
+	var performSearch = function(string, silent) {
 		if (string == "") {
 			if (!silent) {
 				displayError( _self.params.strings.error_empty_field );
@@ -100,12 +127,12 @@ var GMapsLatLonPicker = (function() {
 				}
 			}
 		);
-    };
-    
-    // error function
-    var displayError = function(message) {
-    	alert(message);
-    };
+	};
+	
+	// error function
+	var displayError = function(message) {
+		alert(message);
+	};
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	// PUBLIC FUNCTIONS  //////////////////////////////////////////////////////////////////////////
@@ -125,18 +152,19 @@ var GMapsLatLonPicker = (function() {
 			_self.vars.ID = $(object).attr("id");
 			_self.vars.cssID = "#" + _self.vars.ID + " ";
 
-			_self.params.defLat  = $(_self.vars.cssID + ".gllpLatitude").val()  ? $(_self.vars.cssID + ".gllpLatitude").val()        : _self.params.defLat;
-			_self.params.defLng  = $(_self.vars.cssID + ".gllpLongitude").val() ? $(_self.vars.cssID + ".gllpLongitude").val()       : _self.params.defLng;
-			_self.params.defZoom = $(_self.vars.cssID + ".gllpZoom").val()      ? parseInt($(_self.vars.cssID + ".gllpZoom").val())  : _self.params.defZoom;
+			_self.params.defLat  = $(_self.vars.cssID + ".gllpLatitude").val()  ? $(_self.vars.cssID + ".gllpLatitude").val()		: _self.params.defLat;
+			_self.params.defLng  = $(_self.vars.cssID + ".gllpLongitude").val() ? $(_self.vars.cssID + ".gllpLongitude").val()	    : _self.params.defLng;
+			_self.params.defZoom = $(_self.vars.cssID + ".gllpZoom").val()      ? parseInt($(_self.vars.cssID + ".gllpZoom").val()) : _self.params.defZoom;
 			
 			_self.vars.LATLNG = new google.maps.LatLng(_self.params.defLat, _self.params.defLng);
 
-			_self.vars.MAPOPTIONS        = _self.params.mapOptions;
+			_self.vars.MAPOPTIONS		 = _self.params.mapOptions;
 			_self.vars.MAPOPTIONS.zoom   = _self.params.defZoom;
 			_self.vars.MAPOPTIONS.center = _self.vars.LATLNG; 
 
 			_self.vars.map = new google.maps.Map($(_self.vars.cssID + ".gllpMap").get(0), _self.vars.MAPOPTIONS);
 			_self.vars.geocoder = new google.maps.Geocoder();
+			_self.vars.elevator = new google.maps.ElevationService();
 
 			_self.vars.marker = new google.maps.Marker({
 				position: _self.vars.LATLNG,
@@ -147,19 +175,19 @@ var GMapsLatLonPicker = (function() {
 
 			// Set position on doubleclick
 			google.maps.event.addListener(_self.vars.map, 'dblclick', function(event) {
-		    	setPosition(event.latLng);
-		    });
+				setPosition(event.latLng);
+			});
 		
 			// Set position on marker move
-		    google.maps.event.addListener(_self.vars.marker, 'dragend', function(event) {
-		    	setPosition(_self.vars.marker.position);
-		    });
+			google.maps.event.addListener(_self.vars.marker, 'dragend', function(event) {
+				setPosition(_self.vars.marker.position);
+			});
 	
 			// Set zoom feld's value when user changes zoom on the map
 			google.maps.event.addListener(_self.vars.map, 'zoom_changed', function(event) {
 				$(_self.vars.cssID + ".gllpZoom").val( _self.vars.map.getZoom() );
 				$(_self.vars.cssID).trigger("location_changed", $(_self.vars.cssID));
-		    });
+			});
 
 			// Update location and zoom values based on input field's value 
 			$(_self.vars.cssID + ".gllpUpdateButton").bind("click", function() {
